083516d514f5103123666af320395d01e91297e1
lauanS <lauan.s.souza@gmail.com>
2021-04-08 16:08:59 -0300

    Adição do Header

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,5 @@
 import { Layout, Menu, Button } from 'antd';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { UploadOutlined, LoginOutlined, VideoCameraOutlined } from '@ant-design/icons';
 
 import Map from '../Gmaps/index'
 import './styles.css';
@@ -9,37 +9,37 @@ const { Header, Content, Footer, Sider } = Layout;
 export default function Main_Layout() {
     return(    
     <Layout className="main-layout">
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={broken => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            nav 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            nav 2
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            nav 3
-          </Menu.Item>
-          <Menu.Item key="4" icon={<UserOutlined />}>
-            nav 4
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%', color: 'white' }}>
+        SalvaCão
+      </Header>
+      <Layout className="content-layout">
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<LoginOutlined />}>
+              Login
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              Minhas denúncias
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              Denunciar
+            </Menu.Item>
+          </Menu>
+        </Sider>
         <Content>
           <Map />
         </Content>
-      </Layout>
+      </Layout>      
     </Layout>
     );
 }
