408bda27158888b75b9a2c1ac3abe107b5c4a9b6
lauanS <lauan.s.souza@gmail.com>
2021-04-08 16:07:01 -0300

    Atualização do GMaps
    Alterando o package do google maps

--- src/components/Gmaps/index.js
+++ src/components/Gmaps/index.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import { GoogleApiWrapper, Map } from 'google-maps-react';
+import GoogleMapReact from 'google-map-react';
+
 import apiKey from '../../utils/apiKey';
 
-export class MapContainer extends Component {
-    render() {
-      return (
-        <Map 
-            google={this.props.google} 
-            zoom={14}>
-        </Map>
-      );
-    }
-  }
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-export default GoogleApiWrapper({
-    apiKey: (apiKey())
-})(MapContainer)
+export default class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      <div style={{ height: '100%', width: '100%'}}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: apiKey()}}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
