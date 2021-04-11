112501d4194263b6f7665aa9879ffdffa5787d23
lauanS <lauan.s.souza@gmail.com>
2021-04-11 13:04:55 -0300

    Adicionando uma Searchbox no mapa

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,11 +4,16 @@ import { Button, Drawer } from 'antd';
 import Map from '../../components/Gmaps/index';
 import Layout from '../../components/Layout/index';
 import Marker from '../../components/Marker/index';
-
-
+import SearchBox from '../../components/SearchBox/index';
 
 export default function Main(){
   const [drawerVisible, setDrawerVisible] = useState(false);
+
+  const [mapInstance, setMapInstance] = useState(false);
+  const [mapApi, setMapApi] = useState(false);
+  const [apiReady, setApiReady] = useState(false);
+
+  const [places, setPlaces] = useState([]);
 
   const showDrawer = () => {
     setDrawerVisible(true);
@@ -18,16 +23,26 @@ export default function Main(){
     setDrawerVisible(false);
   };
 
+  const handleApiLoaded = (map, maps) => {
+    if (map && maps) {
+      setMapInstance(map);
+      setMapApi(maps);
+      setApiReady(true);
+      console.log("SET TRUE");
+    }
+    console.log("ESTÁ FALSO");
+  };
+
   return (
     <Layout>
-      <Map>
+      <Map handleApiLoaded={handleApiLoaded}>
         <Marker            
           lat={59.955413}
           lng={30.337844}
           onClick={showDrawer}
         />
       </Map>
-
+      {apiReady && <SearchBox map={mapInstance} mapApi={mapApi} addplace={setPlaces} />}
       <Drawer
         title="Informações sobre a denúncia"
         placement="bottom"
