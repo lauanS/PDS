1d545bad6d33e9eab4e64d5115d201d7cd4ec44f
walo2 <42192595+walo2@users.noreply.github.com>
2021-04-29 03:04:00 -0300

    Finalização do design das telas de login e cadastro

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,6 @@
-import { Form, Input, Button, Checkbox, Row, Col } from 'antd';
 import React from 'react';
+import { Form, Input, Button, Checkbox, Row, Col } from 'antd';
+import { MailOutlined, LockOutlined } from '@ant-design/icons';
 import Layout from '../../components/Layout/index';
 
 import './styles.css';
@@ -7,39 +8,50 @@ import './styles.css';
 export default function Login() {
     return (
         <Layout>
-            <Form
-                name="Login"
-                initialValues={{ remember: true }}
-            >
-                <Form.Item
-                    label="E-mail"
-                    name="email"
-                    rules={[{ required: true, message: 'Por favor, digite o seu endereço de e-mail!' }]}
+            <div className="container">
+                <Form
+                    name="Login"
+                    initialValues={{ remember: true }}
+                    className="login-form"
+                    layout="vertical"
+                    
                 >
-                    <Input />
-                </Form.Item>
+                    <div className="form-container">
+                        <Form.Item
+                            label="E-mail"
+                            name="email"
+                            rules={[{ required: true, message: 'Por favor, digite o seu endereço de e-mail!' }]}
+                        >
+                            <Input
+                                type="email"
+                                prefix={<MailOutlined className="site-form-item-icon" />}
+                                placeholder="Endereço de e-mail"
+                            />
+                        </Form.Item>
 
-                <Form.Item
-                    label="Senha"
-                    name="password"
-                    rules={[{ required: true, message: 'Por favor, digite a senha!' }]}
-                >
-                    <Input.Password />
-                </Form.Item>
+                        <Form.Item
+                            label="Senha"
+                            name="password"
+                            rules={[{ required: true, message: 'Por favor, digite a senha!' }]}
+                        >
+                            <Input.Password
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                placeholder="Senha"
+                            />
+                        </Form.Item>
 
-                <Form.Item name="remember" valuePropName="checked">
-                    <Checkbox>Lembre-se de mim</Checkbox>
-                </Form.Item>
-
-                <Row align="center">
-                    <Form.Item>
-                        <Button type="default" htmlType="submit">
-                            Entrar
+                        <Form.Item name="remember" valuePropName="checked">
+                            <Checkbox>Lembre-se de mim</Checkbox>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Entrar
                     </Button>
-                    </Form.Item>
-                </Row>
-
+                        </Form.Item>
+                    </div>
+                    Não possui registro? <a href="">Cadastre-se!</a>
             </Form>
+            </div>
         </Layout>
     );
 }
