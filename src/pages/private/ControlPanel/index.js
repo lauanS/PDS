8100572bd1d337268e1fa08188c0e816ddbf209b
lauanS <lauan.s.souza@gmail.com>
2021-05-04 16:42:53 -0300

    Visualização das denúncias na parte privada

---
+++ src/pages/private/ControlPanel/index.js
@@ -0,0 +1,130 @@
+import React from "react";
+import { useState, useEffect, useRef, useCallback, useContext } from "react";
+import { Table, Input, Space } from "antd";
+import { Link } from "react-router-dom";
+
+import { getReportsDev } from "../../../services/index";
+import { Context } from "../../../context/authContext";
+
+import "./styles.css";
+const { Search } = Input;
+export default function ControlPanel() {
+  const [reports, setReports] = useState([]);
+  const [filteredReports, setFilteredReports] = useState([]);
+  const [errors, setErrors] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const mounted = useRef(true);
+
+  const { isAuthenticated } = useContext(Context);
+
+  const screenSize = { x: window.innerWidth, y: window.innerHeight };
+
+  const loadReports = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const data = await getReportsDev();
+      if (mounted.current) {
+        let temp = [];
+        data.map((report, key) => {
+          report.key = key;
+          temp.push(report);
+          return;
+        });
+        setReports(temp);
+        setFilteredReports(temp);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      if (mounted.current) {
+        console.log(error);
+        setIsLoading(false);
+        setErrors(true);
+      }
+    }
+    return;
+  }, []);
+
+  const onSearch = (value) => {
+    searchReports(reports, value);
+  };
+
+  const updateSearch = (e) => {
+    setFilteredReports(searchReports(reports, e.target.value) || []);
+  };
+
+  const searchReports = (list, search) => {
+    if (search === "") {
+      return list;
+    }
+
+    return Array.isArray(list)
+      ? list.filter(
+          (report) =>
+            report.address.toLowerCase().indexOf(search.toLowerCase()) !== -1
+        )
+      : [];
+  };
+
+  /* Carregando as denúncias */
+  useEffect(() => {
+    loadReports();
+    return () => {
+      mounted.current = false;
+    };
+  }, [loadReports]);
+
+  /* Colunas da lista */
+  const columns = [
+    {
+      title: "Endereço",
+      dataIndex: "address",
+      key: "address",
+      render: (text) => <Link>{text}</Link>,
+    },
+    {
+      title: "Espécie",
+      dataIndex: "animal",
+      key: "animal",
+      responsive: ["md"],
+    },
+    {
+      title: "Data",
+      dataIndex: "date",
+      key: "date",
+      responsive: ["md"],
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
+    },
+  ];
+
+  return (
+    <>
+      <div className="table-container">
+        <Space direction="vertical" style={{maxWidth: "100%", maxHeight: "100%"}}>
+          <p className="search-title">Busca por endereço</p>          
+          <Search
+            placeholder="Busca por endereço"
+            allowClear
+            onChange={updateSearch}
+            onSearch={onSearch}
+            style={{ width: "100%" }}
+          />
+          <Table
+            columns={columns}
+            dataSource={!isLoading ? filteredReports : null}
+            loading={isLoading}
+            pagination={{ pageSize: Math.floor(screenSize.y / 63) }} // Qtd de itens por pag
+            scroll={{ y: screenSize.y - 280 }} // Tamanho máximo da tabela
+            bordered
+            tableLayout="auto"
+            className="table-reports"
+          />
+        </Space>
+      </div>
+    </>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import Layout from "./components/Layout/index";
+
 import Main from "./pages/Main/index";
 import Login from "./pages/Login/index";
 import SignUp from "./pages/SignUp/index";
 import NotFound from "./pages/NotFound/index";
-import Layout from "./components/Layout/index";
+
+import ControlPanel from "./pages/private/ControlPanel/index";
+
 
 import { AuthProvider } from "./context/authContext";
 
@@ -15,8 +19,10 @@ const Routes = () => (
         <Switch key={"SwitcherRouter"}>
           <Route key={"pag1"} exact path="/" component={Main} />
           <Route key={"pag2"} exact path="/login" component={Login} />  
-          <Route key={"pag3"} exact path="/cadastro" component={SignUp} />  
+          <Route key={"pag3"} exact path="/cadastro" component={SignUp} /> 
+          <Route key={"pag4"} path="/control" component={ControlPanel} /> 
           <Route key={"RDefault"} path="*" component={NotFound} />
+          
         </Switch>
       </Layout>
     </BrowserRouter>
