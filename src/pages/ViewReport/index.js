13d6d255e0f75a7384e9464c2106cc6d7e67b612
lauanS <lauan.s.souza@gmail.com>
2021-05-22 23:15:23 -0300

    Navegação para página da denúncia ao abrir o caso

--- src/pages/ViewReport/index.js
+++ src/pages/ViewReport/index.js
@@ -1,24 +1,17 @@
 import React from "react";
+import { useLocation } from "react-router";
 
 import CardViewReport from "../../components/ViewReport/Card";
 import "./styles.css";
 
-export default function ViewReportPage() {
-  const report = {
-    "id": 1,
-    "description": "O animal está severamente machucado e seu dono se recussa a procurar ajuda, mesmo após a comunidade de unir para pagar um veterinário para o animal",
-    "address": "R. Mituto Mizumoto, 69 - Sé, São Paulo - SP, 01513-040, Brasil",
-    "animal": "Gato",
-    "breeds": "",
-    "status": "processing",
-    "isAnonymous": false,
-    "lat": "-23.557521150854427",
-    "lng": "-46.63197388276037"
-  }
+export default function ViewReportPage(props) {
+  const location = useLocation();
+  const report = location.state.report
   
   return (
     <>
-      <CardViewReport report={report}/>
+      {report? <CardViewReport report={report}/> : <h1>Nenhuma denúncia selecionada</h1>}
+      
     </>
   );
 }
