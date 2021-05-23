b639515e7cb00baa0744375b5702c284ca6c4e82
lauanS <lauan.s.souza@gmail.com>
2021-05-23 16:26:55 -0300

    Melhorias na visualização de datas em um comment

--- src/pages/ViewReport/index.js
+++ src/pages/ViewReport/index.js
@@ -5,58 +5,66 @@ import { useLocation } from "react-router";
 import CardViewReport from "../../components/ViewReport/Card";
 import { getReportCommentsDev } from "../../services";
 
-import moment from 'moment';
+import { parseISO, format, formatRelative } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
 import adminAvatar from "../../assets/admin1.png";
 
 import "./styles.css";
 export default function ViewReportPage(props) {
   const location = useLocation();
-  const report = location.state.report
-  
+  const report = location.state.report;
+
   const [comments, setComments] = useState([]);
 
-  
+  /* Date */
+
   const loadComments = async () => {
     try {
       const data = await getReportCommentsDev(report.id);
       setComments(data);
     } catch (error) {
       console.log(error);
-      message.error("Erro ao carregar os comentários");      
+      message.error("Erro ao carregar os comentários");
     }
     return;
-  }
+  };
 
   return (
     <>
-      {report? 
-      <>
-      <CardViewReport report={report}/> 
-      <Button onClick={loadComments}>Exibir comentários</Button>
+      {report ? (
+        <>
+          <CardViewReport report={report} />
+          <Button onClick={loadComments}>Exibir comentários</Button>
 
-      {comments.map((comment, key) => (
-          <Comment
-            author={<a>Autor da denúncia</a>}
-            content={
-              <p key={key}>{comment.comment}</p>
-            }
-            datetime={              
-              <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-                <span>{moment().fromNow()}</span>
-              </Tooltip>
-            }
-            avatar={
-              <Avatar
-                src={adminAvatar}
-                alt="Han Solo"
-              />
-            }
-            />            
+          {comments.map((comment, key) => (
+            <Comment
+              author={<a>Autor da denúncia</a>}
+              content={<p key={key}>{comment.comment}</p>}
+              datetime={
+                <Tooltip
+                  title={format(
+                    parseISO(comment.date),
+                    "'Dia' dd 'de' MMMM', às ' HH:mm'h'",
+                    { locale: ptBR }
+                  )}
+                >
+                  <span>
+                    {formatRelative(
+                      parseISO(comment.date), 
+                      new Date(), 
+                      { locale: ptBR }
+                    )}
+                  </span>
+                </Tooltip>
+              }
+              avatar={<Avatar src={adminAvatar} alt="Han Solo" />}
+            />
           ))}
-      </>
-      : <h1>Nenhuma denúncia selecionada</h1>}
-      
+        </>
+      ) : (
+        <h1>Nenhuma denúncia selecionada</h1>
+      )}
     </>
   );
 }
