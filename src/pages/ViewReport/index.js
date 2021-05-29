a96727f07c330df8affb339f650586ccc8ddb534
lauanS <lauan.s.souza@gmail.com>
2021-05-29 20:33:36 -0300

    Adição do Editor para criar um comentário

--- src/pages/ViewReport/index.js
+++ src/pages/ViewReport/index.js
@@ -1,20 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router";
+import { Button } from "antd";
 
 import CardViewReport from "../../components/ViewReport/Card";
+import Editor from "../../components/Editor";
 import CommentList from "./CommentList";
 
 import "./styles.css";
 export default function ViewReportPage(props) {
+  const [submitting, setSubmitting] = useState(false);
+  const [value, setValue] = useState("");
+  const [isEditorOpen, setIsEditorOpen] = useState(false);
+
   const location = useLocation();
   const report = location.state.report;
+
+  const handleSubmit = () => {
+    console.log("Comentário enviado");
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const toggleOpenEditor = () => {
+    setIsEditorOpen(!isEditorOpen);
+  };
 
   return (
     <>
       {report ? (
         <>
           <CardViewReport report={report} />
-          <CommentList />          
+          {isEditorOpen ? (
+            <Editor
+              onChange={handleChange}
+              onSubmit={handleSubmit}
+              submitting={submitting}
+              value={value}
+            />
+          ) : (
+            <Button
+              type="text"
+              style={{ display: "block", width: "100%" }}
+              onClick={toggleOpenEditor}
+            >
+              Adicionar um comentário
+            </Button>
+          )}
+          <Button
+            htmlType="submit"
+            loading={submitting}
+            onClick={handleSubmit}
+            type="primary"
+          >
+            Enviar
+          </Button>
+          <CommentList />
         </>
       ) : (
         <h1>Nenhuma denúncia selecionada</h1>
