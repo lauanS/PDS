32f0e3dc0c2d0ea9a1e6b7760fedeb19ddfaff45
lauanS <lauan.s.souza@gmail.com>
2021-06-05 08:45:34 -0300

    Adição do campo de uploading ea lista de arquivos

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,11 +1,23 @@
-import React from "react";
-import { Form, Input } from "antd";
+import React, { useState } from "react";
+import { Form, Input, Upload } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
 
 import "./styles.css";
 
 export default function Editor(props) {
   const { onChange, name, isLoading } = props;
   const { TextArea } = Input;
+  const { fileList, setFileList } = useState([]);
+
+  const normFile = (e) => {
+    console.log('Upload event:', e);
+  
+    if (Array.isArray(e)) {
+      return e;
+    }
+  
+    return e && e.fileList;
+  };
 
   return (
     <>
@@ -26,6 +38,17 @@ export default function Editor(props) {
           onChange={onChange}
           disabled={isLoading}
         />
+      </Form.Item>
+      <Form.Item label="Adicionar Anexos">
+        <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} >
+          <Upload.Dragger name="files" listType="picture-card">
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">Clique ou arraste o arquivo nesta área para envia-los</p>
+            <p className="ant-upload-hint">Suporta o envio de um ou mais arquivos</p>
+          </Upload.Dragger>
+        </Form.Item>
       </Form.Item>
     </>
   );
