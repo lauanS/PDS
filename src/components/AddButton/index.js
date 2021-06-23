6ebc2e4ff0b36dac3d873dc59e389300f861d708
walo2 <42192595+walo2@users.noreply.github.com>
2021-06-23 03:23:00 -0300

    Melhoria no feedback do botão de denunciar

--- src/components/AddButton/index.js
+++ src/components/AddButton/index.js
@@ -2,24 +2,48 @@ import React, { useState } from 'react';
 import { Button, Tooltip } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { CheckOutlined } from '@ant-design/icons';
-
 import './styles.css';
+import { useEffect } from 'react';
 
 export default function AddButton(props) {
   const { onClick, onClickConfirm } = props;
   const [ isSelected, setIsSelected ] = useState(false);
+  const [ showTooltip, setShowTooltip ] = useState(true);
+
   const handleOnClick = () => {
     if(isSelected){
       onClickConfirm();
     } else{
       onClick();
     }
-    setIsSelected(true);    
-  } 
+    setIsSelected(true);
+    setShowTooltip(true);
+  }
+
+  //Esconde tooltip após cinco segundos
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+
+  useEffect(() => {
+    const count = async() => {
+      await delay(7000);
+      console.log(showTooltip)
+      setShowTooltip(false)
+    }
+    count();
+  }, [])
+
+  const toggleVisibility = (value) => {
+    setShowTooltip(value)
+  }
 
   return (
     <div className="fixed-widgets">
-      <Tooltip title={!isSelected ? "Realizar nova denúncia" : "Confirmar Localização"} color="yellow">
+      <Tooltip 
+        title={!isSelected ? "Realizar nova denúncia" : "Confirmar Localização"} 
+        color="#001529"
+        placement="bottomRight"
+        visible={showTooltip}
+        >
         <Button 
           type="default" 
           shape="circle" 
